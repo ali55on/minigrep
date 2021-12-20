5e5a744940346214077f0476e570b21bc099741d
w-a-gomes <wildenalisson@outlook.com>
2021-12-20 19:42:09 -0300

    init

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Procurando por: {}", query);
+    println!("No arquivo: {}", filename);
+
+    let contents = fs::read_to_string(filename)
+        .expect("Algo deu errado ao ler o arquivo");
+
+    println!("Com o texto:\n{}", contents);
 }
+
+// https://doc.rust-lang.org/book/ch12-03-improving-error-handling-and-modularity.html
