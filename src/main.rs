01f31b5eaf3aaa611ff1fc5923cceeb8cd57c35c
w-a-gomes <wildenalisson@outlook.com>
2021-12-22 16:00:13 -0300

    Logic to lib.rs

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,23 @@
 use std::env;
-use std::fs;
+use std::process;
+
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Procurando por: {}", query);
-    println!("No arquivo: {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let contents = fs::read_to_string(filename)
-        .expect("Algo deu errado ao ler o arquivo");
-
-    println!("Com o texto:\n{}", contents);
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
+        process::exit(1);
+    }
 }
 
 // https://doc.rust-lang.org/book/ch12-03-improving-error-handling-and-modularity.html
